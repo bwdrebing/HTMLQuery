99af3f73e28cae2d5709c60f2616ca93bff25908
Ben Drebing <ben.drebing@openfin.co>
2021-06-30 10:58:00 -0400

    added virtual desctructor and removed old script

--- build.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build
-mkdir build
-cmake -S . -B build/ -DCMAKE_TOOLCHAIN_FILE=lib/scripts/buildsystems/vcpkg.cmake
--- src/visitors/IGumboNodeVisitor.h
+++ src/visitors/IGumboNodeVisitor.h
@@ -2,6 +2,7 @@
 
 class IGumboNodeVisitor {
     public:
+        virtual ~IGumboNodeVisitor() = default;
         void iterate(GumboNode* node);
     protected:
         virtual void visit(GumboNode* node) = 0;
